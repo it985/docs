55a60b460d0f2edbff97697fb09153554717e35c
zx <2071916845@qq.com>
2022-12-30 19:29:27 +0800

    关闭 VuePress集成Live2D看板娘，一个沉浸式个性化的vuepress 音乐插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -133,43 +133,43 @@ module.exports = {
       endDate: '2020-04-04 23:59:59' //设置哀悼日的结束日期
     }],
 
-    ['vuepress-plugin-helper-live2d', {
-        // 是否开启控制台日志打印(default: false)
-        log: false,
-        live2d: {
-          // 是否启用(关闭请设置为false)(default: true)
-          enable: true,
-          // 模型名称(default: hibiki)>>>取值请参考：
-          // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
-          model: 'haru/01',
-          display: {
-            position: "right", // 显示位置：left/right(default: 'right')
-            width: 135, // 模型的长度(default: 135)
-            height: 300, // 模型的高度(default: 300)
-            hOffset: 65, //  水平偏移(default: 65)
-            vOffset: 0, //  垂直偏移(default: 0)
-          },
-          mobile: {
-            show: false // 是否在移动设备上显示(default: false)
-          },
-          react: {
-            opacity: 0.8 // 模型透明度(default: 0.8)
-          }
-        }
-      }
-    ],
-    ['vuepress-plugin-immersive-music-player',{
-      sliderStyle:{theme: 'isSpitRainbow'}, //配置播放进度条
-      playListID: 633015043, //网易云歌单ID
-    }]
+    // ['vuepress-plugin-helper-live2d', {
+    //     // 是否开启控制台日志打印(default: false)
+    //     log: false,
+    //     live2d: {
+    //       // 是否启用(关闭请设置为false)(default: true)
+    //       enable: true,
+    //       // 模型名称(default: hibiki)>>>取值请参考：
+    //       // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+    //       model: 'haru/01',
+    //       display: {
+    //         position: "right", // 显示位置：left/right(default: 'right')
+    //         width: 135, // 模型的长度(default: 135)
+    //         height: 300, // 模型的高度(default: 300)
+    //         hOffset: 65, //  水平偏移(default: 65)
+    //         vOffset: 0, //  垂直偏移(default: 0)
+    //       },
+    //       mobile: {
+    //         show: false // 是否在移动设备上显示(default: false)
+    //       },
+    //       react: {
+    //         opacity: 0.8 // 模型透明度(default: 0.8)
+    //       }
+    //     }
+    //   }
+    // ],
+    // ['vuepress-plugin-immersive-music-player',{
+    //   sliderStyle:{theme: 'isSpitRainbow'}, //配置播放进度条
+    //   playListID: 633015043, //网易云歌单ID
+    // }],
   ],
 
   // 插件
   plugins: [
 
     ['graysite'],//使您的网站变灰的插件
-    ['vuepress-plugin-helper-live2d'], //VuePress集成Live2D看板娘
-    ['vuepress-plugin-immersive-music-player'],//一个沉浸式个性化的vuepress 音乐插件
+    // ['vuepress-plugin-helper-live2d'], //VuePress集成Live2D看板娘
+    // ['vuepress-plugin-immersive-music-player'],//一个沉浸式个性化的vuepress 音乐插件
     [require('./plugins/love-me'), { // 鼠标点击爱心特效
       color: '#11a8cd', // 爱心颜色，默认随机色
       excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
